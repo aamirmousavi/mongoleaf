2533da81645ef623b9dcc04f22b964db0dec9aa3
Amir Mousavi <38921349+aamirmousavi@users.noreply.github.com>
2022-01-11 21:59:47 +0330

    Update mongo functions
    added options for countDocuments, distinct, findOneAndUpdate, findOneAndReplace, findOneAndDelete, createCollection, createView, listCollections, listDatabaseNames, session, runCmd.

--- options.go
+++ options.go
@@ -67,3 +67,95 @@ func (_ optionType) findOne(option string) (options.FindOneOptions, error) {
 	}
 	return op, nil
 }
+func (_ optionType) countDocuments(option string) (options.CountOptions, error) {
+	var op options.CountOptions
+	err := json.Unmarshal([]byte(option), &op)
+	if err != nil {
+		return options.CountOptions{}, err
+	}
+	return op, nil
+}
+func (_ optionType) distinct(option string) (options.DistinctOptions, error) {
+	var op options.DistinctOptions
+	err := json.Unmarshal([]byte(option), &op)
+	if err != nil {
+		return options.DistinctOptions{}, err
+	}
+	return op, nil
+}
+func (_ optionType) findOneAndUpdate(option string) (options.FindOneAndUpdateOptions, error) {
+	var op options.FindOneAndUpdateOptions
+	err := json.Unmarshal([]byte(option), &op)
+	if err != nil {
+		return options.FindOneAndUpdateOptions{}, err
+	}
+	return op, nil
+}
+func (_ optionType) findOneAndReplace(option string) (options.FindOneAndReplaceOptions, error) {
+	var op options.FindOneAndReplaceOptions
+	err := json.Unmarshal([]byte(option), &op)
+	if err != nil {
+		return options.FindOneAndReplaceOptions{}, err
+	}
+	return op, nil
+}
+func (_ optionType) findOneAndDelete(option string) (options.FindOneAndDeleteOptions, error) {
+	var op options.FindOneAndDeleteOptions
+	err := json.Unmarshal([]byte(option), &op)
+	if err != nil {
+		return options.FindOneAndDeleteOptions{}, err
+	}
+	return op, nil
+}
+
+//
+
+func (_ optionType) createCollection(option string) (options.CreateCollectionOptions, error) {
+	var op options.CreateCollectionOptions
+	err := json.Unmarshal([]byte(option), &op)
+	if err != nil {
+		return options.CreateCollectionOptions{}, err
+	}
+	return op, nil
+}
+func (_ optionType) createView(option string) (options.CreateViewOptions, error) {
+	var op options.CreateViewOptions
+	err := json.Unmarshal([]byte(option), &op)
+	if err != nil {
+		return options.CreateViewOptions{}, err
+	}
+	return op, nil
+}
+func (_ optionType) listCollections(option string) (options.ListCollectionsOptions, error) {
+	var op options.ListCollectionsOptions
+	err := json.Unmarshal([]byte(option), &op)
+	if err != nil {
+		return options.ListCollectionsOptions{}, err
+	}
+	return op, nil
+}
+
+func (_ optionType) listDatabaseNames(option string) (options.ListDatabasesOptions, error) {
+	var op options.ListDatabasesOptions
+	err := json.Unmarshal([]byte(option), &op)
+	if err != nil {
+		return options.ListDatabasesOptions{}, err
+	}
+	return op, nil
+}
+func (_ optionType) session(option string) (options.SessionOptions, error) {
+	var op options.SessionOptions
+	err := json.Unmarshal([]byte(option), &op)
+	if err != nil {
+		return options.SessionOptions{}, err
+	}
+	return op, nil
+}
+func (_ optionType) runCmd(option string) (options.RunCmdOptions, error) {
+	var op options.RunCmdOptions
+	err := json.Unmarshal([]byte(option), &op)
+	if err != nil {
+		return options.RunCmdOptions{}, err
+	}
+	return op, nil
+}
